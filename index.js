bda3e9d2277c05c6baef3f8072a1645b3606c403
Silvana <silvana_dark@hotmail.com>
2023-11-27 22:38:43 -0300

    Calculadora

---
+++ index.js
@@ -0,0 +1,29 @@
+let vitoria = 80
+let derrota = 30
+
+let resultado = calcularNivel(vitoria, derrota);
+
+console.log("O Herói tem um saldo de " + resultado.saldoVitorias + " está no nível de " + resultado.nivel);
+
+function calcularNivel(vitorias, derrotas) {
+    var saldoVitorias = vitorias - derrotas;
+    var nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
